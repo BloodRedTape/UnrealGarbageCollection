4a4ab9a8c50bf35efff5075b0269ceccfd3bfab3
BloodRedTape <gefestroyale@gmail.com>
2022-06-15 23:11:55 +0300

    U2Shooter: Base movement

---
+++ U2Shooter/Source/Shooter/Entities/FirstPersonCharacter.cpp
@@ -0,0 +1,43 @@
+﻿#include "FirstPersonCharacter.h"
+
+AFirstPersonCharacter::AFirstPersonCharacter(){
+	PrimaryActorTick.bCanEverTick = true;
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerViewCamera"));
+}
+void AFirstPersonCharacter::PostActorCreated(){
+	Super::PostActorCreated();
+	
+	Camera->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	Camera->SetRelativeLocation(FVector(0, 0, 40));
+	Camera->bUsePawnControlRotation = true;
+}
+
+void AFirstPersonCharacter::BeginPlay(){
+	Super::BeginPlay();
+}
+
+void AFirstPersonCharacter::Tick(float DeltaTime){
+	Super::Tick(DeltaTime);
+}
+
+void AFirstPersonCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent){
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("Forward", this, &AFirstPersonCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("Right", this, &AFirstPersonCharacter::MoveRight);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAxis("Yaw", this, &AFirstPersonCharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("Pitch", this, &AFirstPersonCharacter::AddControllerPitchInput);
+}
+
+void AFirstPersonCharacter::MoveForward(float Value){
+	AddMovementInput(GetActorForwardVector(), Value);
+}
+	
+void AFirstPersonCharacter::MoveRight(float Value){
+	AddMovementInput(GetActorRightVector(), Value);
+}
+
+void AFirstPersonCharacter::MovePitch(float Value){
+	Camera->SetRelativeRotation(Camera->GetRelativeRotation() + FRotator(Value, 0, 0));
+}
---
+++ U2Shooter/Source/Shooter/Entities/FirstPersonCharacter.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/Character.h"
+#include "FirstPersonCharacter.generated.h"
+
+UCLASS()
+class SHOOTER_API AFirstPersonCharacter : public ACharacter{
+	GENERATED_BODY()
+private:
+	UPROPERTY(EditAnywhere, Category="FirstPersonCharacter")
+	UCameraComponent *Camera;
+public:
+	AFirstPersonCharacter();
+
+	virtual void PostActorCreated() override;
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	void MoveForward(float Value);
+	
+	void MoveRight(float Value);
+
+	void MovePitch(float Value);
+};
